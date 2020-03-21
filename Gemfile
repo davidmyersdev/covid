10e4d267effbb428dbfff0b945dd73c483456bb8
David R. Myers <davidrmyersii@gmail.com>
2020-03-20 22:45:18 -0400

    Add tweet script

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org' do
+  gem 'dotenv'
+  gem 'httparty'
+  gem 'twitter'
+end
---
+++ tweet.rb
@@ -0,0 +1,32 @@
+require 'bundler/setup'
+require 'dotenv/load'
+require 'httparty'
+require 'rubygems'
+require 'twitter'
+
+response = HTTParty.get('https://corona.lmao.ninja/states')
+ohio = response.parsed_response.find { |item| item['state'] == 'Ohio' }
+
+abort 'Ohio not found' unless ohio
+
+cases = ohio['cases']
+deaths = ohio['deaths']
+recovered = ohio['recovered']
+
+abort 'ohio[cases] missing' if cases.nil?
+abort 'ohio[deaths] missing' if deaths.nil?
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.access_token = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
+
+client.update <<~TWEET
+  COVID update.
+
+  Total Ohio cases: #{cases}
+  Total Ohio recovered: #{recovered}
+  Total Ohio deaths: #{deaths}
+TWEET
